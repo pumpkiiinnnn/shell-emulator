b25c8add89b0480551e4d6297f3996f9e460db26
pumpkiiinnnn <beltukova15@gmail.com>
2025-09-07 05:22:46 -0700

    Update main.py
    добавили команду echo и обработку случая с неизвестной командой

--- src/main.py
+++ src/main.py
@@ -6,9 +6,13 @@ hostname=socket.gethostname()
 while True:
     prompt=f"{username}@{hostname}:~$ "
     command = input(prompt)
+    parts=command.split()
 
     if command.strip() == "exit":
         print("Выход из эмулятора...")
         break
-    print("Вы ввели:", command)
-
+    #print("Вы ввели:", command)
+    elif parts[0]== "echo":
+        print(" ".join(parts[1:]))
+    else:
+        print("Command not found")
