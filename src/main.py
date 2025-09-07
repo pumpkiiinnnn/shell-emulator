acadb1b4c06c29a2becc472186488730d72a5c95
pumpkiiinnnn <beltukova15@gmail.com>
2025-09-07 04:59:23 -0700

    Update main.py
    создан минимальный эмулятор с командой exit

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
+import getpass
+import socket
+username=getpass.getuser()
+hostname=socket.gethostname()
+
+while True:
+    prompt=f"{username}@{hostname}:~$ "
+    command = input(prompt)
+
+    if command.strip() == "exit":
+        print("Выход из эмулятора...")
+        break
+    print("Вы ввели:", command)
 
